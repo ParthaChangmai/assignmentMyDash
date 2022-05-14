b1c474e81094da3bf9f721fbd543c5e14c1ee637
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-05-14 06:53:23 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Home from './components/home/Home';
 import './Global.scss';
+import { Routes, Route } from 'react-router-dom';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
 	return (
 		<>
-			<Home />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/*" element={<NotFound />} />
+			</Routes>
 		</>
 	);
 }
