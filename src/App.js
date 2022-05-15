2e5fb35ca9861fc6b389ae84f4fe531ca740cbf0
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-05-15 14:32:31 +0530

    added chart route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Home from './components/home/Home';
 import './Global.scss';
 import { Routes, Route } from 'react-router-dom';
 import NotFound from './components/notFound/NotFound';
+import Chart from './components/chart/Chart';
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/chart" element={<Chart />} />
 				<Route path="/*" element={<NotFound />} />
 			</Routes>
 		</>
