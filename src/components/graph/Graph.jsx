b5769dc55a13df7d197d4a2535066e44f4f92707
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-05-16 17:10:29 +0530

    removed the barchart

--- src/components/graph/Graph.jsx
+++ src/components/graph/Graph.jsx
@@ -1,79 +1,28 @@
-import React, { useRef, useEffect, useState } from 'react';
-import * as d3 from 'd3';
+import React, { useState } from 'react';
+
 import '../../styles/Graph.scss';
+import BarChartD from './BarChartD';
 
 const Graph = () => {
-	const randomArray = (length, max) =>
+	const randomData = (length, max) =>
 		[...new Array(length)].map(() => Math.round(Math.random() * max));
 
-	const d3Ref = useRef(null);
-	const [dataset, setDataset] = useState([
-		100, 500, 200, 300, 400, 200, 400, 600, 400, 200,
+	const [data, setData] = useState([
+		100, 500, 200, 300, 400, 200, 400, 600, 400, 200, 400, 200, 420, 650, 200,
 	]);
 
 	const changeData = (e) => {
 		e.preventDefault();
-		const data = randomArray(10, 500);
-		setDataset(data);
+		const data = randomData(15, 500);
+		setData(data);
 	};
 
-	useEffect(() => {
-		const size = 500;
-		const svg = d3
-			.select(d3Ref.current)
-			.append('svg')
-			.attr('width', 1060)
-			.attr('height', size);
-
-		const rect_width = 95;
-
-		svg
-			.selectAll(`rect`)
-
-			.data(dataset)
-			.enter()
-			.append(`rect`)
-
-			.attr('x', (d, i) => 10 + i * (rect_width + 10))
-			.attr('y', (d) => size - d)
-
-			.transition()
-			.duration(200)
-
-			.attr(`width`, rect_width)
-			.attr(`height`, (d) => d)
-
-			.attr('fill', () => {
-				return 'hsl(' + Math.random() * 70 + ',100%,50%)';
-			});
-
-		svg
-
-			.selectAll(`text`)
-
-			.data(dataset)
-			.enter()
-			.append(`text`)
-			.text(function (d) {
-				return dataset[0];
-			})
-			.attr('x', rect_width / 2.2)
-			.attr('y', size - 5)
-			.attr('fill', 'black')
-			.attr('font-family', 'Times New Roman')
-			.attr('font-weight', 'bold')
-			.attr('fill', 'black');
-
-		return () => {
-			svg.remove('svg');
-		};
-	}, [dataset]);
-
 	return (
-		<div id="d3Graph" ref={d3Ref} className="graph">
+		<div className="graph">
 			<button type="button" className="graph__button" onClick={changeData}>
 				Randomize Data
 			</button>
+			<BarChartD width={700} height={400} data={data} />
 		</div>
 	);
 };
